b11c7b4c8787cb8985a7b91333cf7a3f351dbf42
ni391919-ship-it <ni391919@gmail.com>
2026-04-07 02:17:38 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({//Github pages configaration
   plugins: [react(), tailwindcss(), viteSingleFile()],
   
   resolve: {base:'/Hangout Planner'
